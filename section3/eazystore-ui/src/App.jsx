1d51ec816368cc3dabaf9c4bc4f23a80b96fdb0d
Shen Tang <adatang23@yahoo.com>
2026-03-21 17:12:48 -0400

    Added parent and children components with props, passing props from parent to children

--- section3/eazystore-ui/src/App.jsx
+++ section3/eazystore-ui/src/App.jsx
@@ -2,11 +2,13 @@ import Header from './components/Header';
 import ClassHeader from './components/ClassHeader';
 import Footer from './components/footer/Footer';
 import React from "react";
+import Home from './components/Home';
 
 function App() {
   return (
     <>
       <Header/>
+      <Home/>
       <Footer/>
     </>
   );
