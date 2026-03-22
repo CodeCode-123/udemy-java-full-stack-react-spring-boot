41e44e80cb76c9a4343ed723b89e106d1bd13570
Shen Tang <adatang23@yahoo.com>
2026-03-22 18:13:51 -0400

    Added Bootstrap styles

--- section4/eazystore-ui/src/App.jsx
+++ section4/eazystore-ui/src/App.jsx
@@ -3,6 +3,9 @@ import ClassHeader from './components/ClassHeader';
 import Footer from './components/footer/Footer';
 import React from "react";
 import Home from './components/Home';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./custom.scss";
 
 function App() {
   return (
--- section4/eazystore-ui/src/components/Header.jsx
+++ section4/eazystore-ui/src/components/Header.jsx
@@ -10,22 +10,22 @@ export default function Header() {
                     <FontAwesomeIcon icon={faTags} className="fa-icon" />
                     <span className="brand-title">Eazy Stickers</span>
                 </a>
-                <nav className="nav">
+                <nav className="eazynav">
                     <ul>
                         <li>
-                            <a href="/" className="nav-link">Home</a>
+                            <a href="/" className="navLink">Home</a>
                         </li>
                     <li>
-                        <a href="/about" className="nav-link">About</a>
+                        <a href="/about" className="navLink">About</a>
                         </li>
                         <li>
-                            <a href="/contact" className="nav-link">Contact</a>
+                            <a href="/contact" className="navLink">Contact</a>
                         </li>
                         <li>
-                            <a href="/login" className="nav-link">Login</a>
+                            <a href="/login" className="navLink">Login</a>
                         </li>
                         <li>
-                            <a href="/cart" className="nav-link">
+                            <a href="/cart" className="navLink">
                                 <FontAwesomeIcon icon={faShoppingBasket} />
                             </a>
                         </li>
