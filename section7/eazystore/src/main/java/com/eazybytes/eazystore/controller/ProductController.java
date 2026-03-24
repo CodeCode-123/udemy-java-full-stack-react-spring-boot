feb8e39f37d1f7ac803dc2322442b7669fbd1504
Shen Tang <adatang23@yahoo.com>
2026-03-24 18:26:10 -0400

    Updated ProductController with DTO and Added CorsConfig

--- section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor // without writing constructor with args
+//@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
     private final IProductService productService;
-//    @Autowired
+//    @Autowired not necessary if use @RequiredArgsConstructor
 //    public ProductController(IProductService productService) {
 //        this.productService = productService;
 //    }
