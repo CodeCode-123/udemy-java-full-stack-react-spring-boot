0dcb715b1f507a9d331808db18dc3b4c4af38748
Shen Tang <adatang23@yahoo.com>
2026-03-25 01:28:59 -0400

    Added React Hooks by using useState, useMemo, and useEffect

--- section8/eazystore-ui/src/components/ProductListings.jsx
+++ section8/eazystore-ui/src/components/ProductListings.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import ProductCard from './ProductCard';
 import SearchBox from './SearchBox';
 import Dropdown from './Dropdown';
@@ -6,11 +6,39 @@ import Dropdown from './Dropdown';
 const sortList = ["Popularity", "Price Low to High", "Price High to Low"]; 
 
 export default function ProductListings({ products }) {
-  let searchText = "";
-  function handleSearchChange(inputSearch, event) {
-    searchText = inputSearch;
-    console.log(searchText);
-    console.log(event);
+  // use Hooks, set initial value, useState("")
+  const [searchText, setSearchText] = useState("");
+  const [selectedSort, setSelectedSort] = useState("Popularity");
+  
+  const filteredAndSortedProducts = useMemo(() => {
+        if (!Array.isArray(products)) {
+            return [];
+        }
+        let filteredProducts = products.filter(
+            (product) =>
+                product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+                product.description.toLowerCase().includes(searchText.toLowerCase())
+        );
+        return filteredProducts.slice().sort((a, b) => {
+            switch (selectedSort) {
+                case "Price Low to High":
+                    return parseFloat(a.price) - parseFloat(b.price);
+                case "Price High to Low":
+                    return parseFloat(b.price) - parseFloat(a.price);
+                case "Popularity":
+                default:
+                    return parseInt(b.popularity) - parseInt(a.popularity);
+            }
+        });
+    }, [products, searchText, selectedSort]);
+
+  function handleSearchChange(inputSearch) {
+    // setSearchText to allow input typing display
+    setSearchText(inputSearch);
+  }
+
+  function handleSortChange(sortType) {
+    setSelectedSort(sortType);
   }
 
   return (
@@ -22,11 +50,17 @@ export default function ProductListings({ products }) {
               value={searchText} 
               handleSearch={(value, event) => handleSearchChange(value, event)}
             />
-            <Dropdown label="Sort by" options={sortList} value="Popularity" />
+            <Dropdown label="Sort by" 
+              options={sortList} 
+              value={selectedSort} 
+              handleSort={(value) => handleSortChange(value)} 
+            />
         </div>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12'>
-            {products.length > 0 ? (
-                products.map((product) => <ProductCard key={product.productId} product={product} />)
+            {filteredAndSortedProducts.length > 0 ? (
+                filteredAndSortedProducts.map((product) => (
+                <ProductCard key={product.productId} product={product} />
+                ))
             ) : (
                 <p className="text-center font-primary font-bold text-lg text-primary">
                     No products found
