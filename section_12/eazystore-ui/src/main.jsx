647059e83cfd9c38629cb914f95488129a2d254c
Shen Tang <adatang23@yahoo.com>
2026-03-29 14:11:51 -0400

    Added useContext() for the child components to consume context from a parent without passing props through every intermediate level

---
+++ section_12/eazystore-ui/src/main.jsx
@@ -0,0 +1,65 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import Login from './components/Login.jsx'
+import Cart from './components/Cart.jsx'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import ErrorPage from './components/ErrorPage.jsx';
+import { productsLoader } from "./components/Home.jsx";
+import { contactAction } from './components/Contact.jsx';
+import { ToastContainer, Bounce } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import ProductDetail from './components/ProductDetail.jsx';
+import { CartContext } from "./store/cart-context.jsx";
+
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App/>} errorElement={<ErrorPage/>}>
+    <Route index element={<Home />} loader={productsLoader} />
+    <Route path="/home" element={<Home />} loader={productsLoader} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} action={contactAction} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/products/:productId" element={<ProductDetail />} />
+  </Route>
+);
+
+const appRouter = createBrowserRouter(routeDefinitions);
+
+const initialCartContext = {
+  cart: [],
+  setCart: () => {},
+  addToCart: () => {
+    console.log("Product added to cart");
+  },
+  removeFromCart: () => {},
+  totalQuantity: 0,
+};
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* .Provider can be omitted, only use <CartContext></CartContext> */}
+    <CartContext.Provider value={initialCartContext}> 
+      {/* the value can be shared by all the children of CartContext, by wrapping 
+      RouterProvider all the components are becoming the child components 
+      of the CartContext */}
+      <RouterProvider router={appRouter}/>
+    </CartContext.Provider>
+    <ToastContainer
+    position='top-center'
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    draggable
+    pauseOnHover
+    theme={localStorage.getItem("theme") === "dark" ? "dark":"light"}
+    transition={Bounce} />
+  </StrictMode>,
+)
+
+
+
