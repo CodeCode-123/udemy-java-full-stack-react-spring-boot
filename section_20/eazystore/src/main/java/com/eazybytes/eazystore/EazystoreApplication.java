918616e48ea7aaaa1e515517bf3dfd452d49ea94
Shen Tang <adatang23@yahoo.com>
2026-04-11 20:13:03 -0400

    Added Caching with @Cacheable annotation to the repository or service layer; @CachePut and @CacheEvict can be used during updates or deletions. Added Caffeine Maven dependency to cache with TTL (Time-To-Live) instead of ConcurrentHashMap without TTL. Used the actuator to monitor caches

--- section_20/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ section_20/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -3,6 +3,7 @@ package com.eazybytes.eazystore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 // if import spring-boot-starter-data-jpa as a Maven dependency,
 // The DataSourceAutoConfiguration.class can be excluded
 @SpringBootApplication
+@EnableCaching
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 //@ComponentScan(basePackages = {"com.eazybytes.eazystore.controller"})
 //@EnableJpaRepositories // Optional if using entity and repository in the main folder
