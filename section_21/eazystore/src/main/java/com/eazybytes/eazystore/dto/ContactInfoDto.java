8ae7d4533e9f99f01e740ba6855ec2c0aa13072b
Shen Tang <adatang23@yahoo.com>
2026-05-20 12:00:48 -0400

    Added Reading properties using 1) @value annotation, 2) Environment env.getProperty("property.name"), 3) @propertysource annotation

--- section_21/eazystore/src/main/java/com/eazybytes/eazystore/dto/ContactInfoDto.java
+++ section_21/eazystore/src/main/java/com/eazybytes/eazystore/dto/ContactInfoDto.java
@@ -1,4 +1,7 @@
 package com.eazybytes.eazystore.dto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-public record ContactInfoDto() {
+//the annotation binds external configuration values from application.properties
+@ConfigurationProperties("contact")
+public record ContactInfoDto(String phone, String email, String address) {
 }
