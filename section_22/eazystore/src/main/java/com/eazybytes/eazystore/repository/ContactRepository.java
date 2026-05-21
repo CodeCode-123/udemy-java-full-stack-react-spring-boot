9f3df17d5de57d3514a285e2737696c975af166d
Shen Tang <adatang23@yahoo.com>
2026-05-21 13:38:50 -0400

    Added Redux instead of React Context API to handle global state data, imported Redux, added cart-slice.js and auth-slice.js to create slices of Redux. Added store.js to handle cartReducer and authReducer. Modified the frontend to use Redux to handle global states of Cart, such as addToCart, removeFromCart, and clearCart, the global states of auth like LoginSuccess, Logout, user, isAuthenticated, and JWT token.

---
+++ section_22/eazystore/src/main/java/com/eazybytes/eazystore/repository/ContactRepository.java
@@ -0,0 +1,19 @@
+package com.eazybytes.eazystore.repository;
+
+import com.eazybytes.eazystore.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository // Optional
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    // the method name findByStatus is the same as NamedQuery(name = "Contact.findByStatus")
+    List<Contact> findByStatus(String status);
+
+    @Query(name = "Contact.findByStatus")
+    List<Contact> fetchByStatus(String status);
+
+    List<Contact> findByStatusWithNativeQuery(String status);
+}
