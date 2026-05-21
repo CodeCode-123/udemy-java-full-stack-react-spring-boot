69bdbecebfab1530732c0c5344b2caf274243f90
Shen Tang <adatang23@yahoo.com>
2026-05-21 16:15:39 -0400

    Configured the frontend React project for Cloud deployment, added dev and production environments, and added build to the vite.config.js

--- section_23/eazystore-ui/vite.config.js
+++ section_23/eazystore-ui/vite.config.js
@@ -5,4 +5,30 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('react') || id.includes('react-dom')) {
+            return 'vendor';
+          }
+          if (id.includes('@reduxjs/toolkit') || id.includes('react-redux')) {
+            return 'redux';
+          }
+          if (id.includes('react-router-dom')) {
+            return 'router';
+          }
+          if (id.includes('@fortawesome/react-fontawesome') || id.includes('@fortawesome/fontawesome-svg-core')) {
+            return 'ui';
+          }
+        },
+      },
+    },
+  },
+  base: "/",
+  server: { port: 5173 },
+  preview: { port: 5173 }
+});
